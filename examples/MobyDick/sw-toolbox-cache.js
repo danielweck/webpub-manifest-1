a85af370b09fd62159e2dc5bca41af69b2f5df32
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-05 04:12:48 +0200

    Fixed typo in SW

--- examples/MobyDick/sw-toolbox-cache.js
+++ examples/MobyDick/sw-toolbox-cache.js
@@ -7,7 +7,7 @@
   // Debug mode
   global.toolbox.options.debug = true;
 
-  AppManifest = '{"name": "Moby-Dick; or, The Whale", "short_name": "Moby-Dick", "start_url": "index.html", "display": "standalone", "icons": [{"src": "icon-large.png", "sizes": "192x192", "type": "image/png"}]}'
+  var AppManifest = '{"name": "Moby-Dick; or, The Whale", "short_name": "Moby-Dick", "start_url": "index.html", "display": "standalone", "icons": [{"src": "icon-large.png", "sizes": "192x192", "type": "image/png"}]}'
 
   toolbox.precache([
       'index.html',
@@ -29,7 +29,7 @@
     ]);
 
   toolbox.router.get('appmanifest.json', function(request) {
-    return new Response('{"name": "Moby Dick"}');
+    return new Response(AppManifest);
   });
 
   global.toolbox.router.default = global.toolbox.fastest;
