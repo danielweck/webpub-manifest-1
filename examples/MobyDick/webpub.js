14de1fb8d04b3c3a5a0695eeb4206879c62bd375
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-06 16:51:27 +0200

    Only register SW if available in browser

--- examples/MobyDick/webpub.js
+++ examples/MobyDick/webpub.js
@@ -2,7 +2,8 @@
 Very early demo, that does two things:
 - cache resources necessary for reading the publication offline
 - generates a Web App Manifest (if you activate the sw-toolbox variant)
-This is now done dynamically but too often, will need to figure out a way to trigger this event only once.
+
+This script checks if the manifest file is stored in the cache, and only precaches resources if it isn't.
 
 Check the full list of expected features at: 
 https://github.com/HadrienGardeur/webpub-manifest/wiki/Web-Publication-JS
@@ -10,45 +11,39 @@ https://github.com/HadrienGardeur/webpub-manifest/wiki/Web-Publication-JS
 
 (function() {
 
-  var installingMsg = document.querySelector('.installing-msg');
-  if (!navigator.serviceWorker) {
-    installingMsg.textContent = "Service worker not supported";
-    return;
+  if (navigator.serviceWorker) {
+    //Basic SW
+    navigator.serviceWorker.register('sw.js');
+
+    //SW based on sw-toolbox that also generates the Web App Manifest
+    //navigator.serviceWorker.register('sw-toolobox-cache.js');
+  
+    navigator.serviceWorker.ready.then(function() {
+      console.log('SW ready');
+    }); 
   }
-  
-  //Basic SW
-  navigator.serviceWorker.register('sw.js');
 
-  //SW based on sw-toolbox that also generates the Web App Manifest
-  //navigator.serviceWorker.register('sw-toolobox-cache.js');
-  
-  navigator.serviceWorker.ready.then(function() {
-    console.log('SW ready');
-  });
+  var manifest_url = document.querySelector("link[rel='manifest'][type='application/webpub+json']").href
 
-  //if (navigator.serviceWorker.controller) {
-  	var manifest_url = document.querySelector("link[rel='manifest'][type='application/webpub+json']").href
-
-    if (manifest_url) {
+  if (manifest_url) {
       
-      caches.open(manifest_url).then(function(cache) {
-        return cache.match(manifest_url).then(function(response){
-          if (!response) {
-            console.log("No cache key found");
-            console.log('Caching manifest at:'+manifest_url);
-            var webpub = getManifest(manifest_url)
-            cacheSpine(webpub);
-            cacheResources(webpub);
-          } else {
-            console.log("Found cache key");
-          }
-        })
-      });
+    caches.open(manifest_url).then(function(cache) {
+      return cache.match(manifest_url).then(function(response){
+        if (!response) {
+          console.log("No cache key found");
+          console.log('Caching manifest at:'+manifest_url);
+          var webpub = getManifest(manifest_url)
+          cacheSpine(webpub);
+          cacheResources(webpub);
+        } else {
+          console.log("Found cache key");
+        }
+      })
+    });
       
-    } else {
-      console.log('No Web Publication Manifest detected');
-    }
-  //};
+  } else {
+    console.log('No Web Publication Manifest detected');
+  }
 
   function getManifest(url) {
     return fetch(url).then(function(response) {
