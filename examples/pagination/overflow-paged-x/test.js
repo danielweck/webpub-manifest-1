0a94723cf5549c92760b146a70d6e8af7c7b9338
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-19 01:32:15 +0200

    Limit to click event

--- examples/pagination/overflow-paged-x/test.js
+++ examples/pagination/overflow-paged-x/test.js
@@ -2,35 +2,9 @@
 
 (function() {
 
-  //var mc = new Hammer(document.body);
   var page = 0;
 
-  //mc.on("swipeleft, tap", function(event) {
-  //  page = page+1;
-  //  document.body.scrollLeft = page*(window.outerWidth);
-  //});
-
-
-
-  //mc.on("swiperight", function(event) {
-  //  if (page>0) page = page+1;
-  //  document.body.scrollLeft = page*(window.outerWidth);
-  //});
-
-  //window.addEventListener("click", (function(e) {        
-  //  var clickX = e.pageX;
-  //  console.log("Click detected at: "+clickX);
-  //  console.log((window.innerWidth*page)+(window.innerWidth/2));
-  //  if (clickX > (window.innerWidth*page)+(window.innerWidth/2)) {
-  //    page = page+1;
-  //    document.body.scrollLeft = page*(window.outerWidth);
-  //  } else {
-  //    if (page>0) page = page-1;
-  //    document.body.scrollLeft = page*(window.outerWidth);
-  //  }
-  //}));
-
-  window.addEventListener("touchend", (function(e) {        
+  window.addEventListener("click", (function(e) {        
     var clickX = e.pageX;
     console.log("Click detected at: "+clickX);
     console.log((window.innerWidth*page)+(window.innerWidth/2));
