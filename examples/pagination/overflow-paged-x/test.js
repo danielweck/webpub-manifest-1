da37a5a25da3639b3e419efaff9ef0e6f00cb184
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-19 01:26:13 +0200

    Trying to improve events on mobile devices

--- examples/pagination/overflow-paged-x/test.js
+++ examples/pagination/overflow-paged-x/test.js
@@ -18,9 +18,10 @@
   //});
 
   window.addEventListener("click", (function(e) {        
-    var clickX = e.clientX;
+    var clickX = e.pageX;
     console.log("Click detected at: "+clickX);
-    if (clickX > (window.innerWidth/2)) {
+    console.log((window.innerWidth*page)+(window.innerWidth/2));
+    if (clickX > (window.innerWidth*page)+(window.innerWidth/2)) {
       page = page+1;
       document.body.scrollLeft = page*(window.outerWidth);
     } else {
@@ -29,8 +30,21 @@
     }
   }));
 
-  window.addEventListener("scroll", (function(e) {        
-    document.body.scrollLeft = page*(window.outerWidth);
+  window.addEventListener("touchend", (function(e) {        
+    var clickX = e.pageX;
+    console.log("Click detected at: "+clickX);
+    console.log((window.innerWidth*page)+(window.innerWidth/2));
+    if (clickX > (window.innerWidth*page)+(window.innerWidth/2)) {
+      page = page+1;
+      document.body.scrollLeft = page*(window.outerWidth);
+    } else {
+      if (page>0) page = page-1;
+      document.body.scrollLeft = page*(window.outerWidth);
+    }
   }));
+
+  //window.addEventListener("scroll", (function(e) {        
+  //  document.body.scrollLeft = page*(window.outerWidth);
+  //}));
 
 }());
