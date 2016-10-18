967f60fda6e30c6220ebf2d1f3d1a9cdd5d405eb
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-19 00:47:19 +0200

    New experiment with support for taps in both directions

--- examples/pagination/overflow-paged-x/test.js
+++ examples/pagination/overflow-paged-x/test.js
@@ -2,17 +2,35 @@
 
 (function() {
 
-  var mc = new Hammer(document.body);
+  //var mc = new Hammer(document.body);
   var page = 0;
 
-  mc.on("swipeleft, tap", function(event) {
-    page = page+1;
-    document.body.scrollLeft = page*(window.outerWidth);
-  });
+  //mc.on("swipeleft, tap", function(event) {
+  //  page = page+1;
+  //  document.body.scrollLeft = page*(window.outerWidth);
+  //});
+
+
 
   //mc.on("swiperight", function(event) {
   //  if (page>0) page = page+1;
   //  document.body.scrollLeft = page*(window.outerWidth);
   //});
+
+  window.addEventListener("click", (function(e) {        
+    var clickX = e.clientX;
+    console.log("Click detected at: "+clickX);
+    if (clickX > (window.outerWidth/2)) {
+      page = page+1;
+      document.body.scrollLeft = page*(window.outerWidth);
+    } else {
+      if (page>0) page = page-1;
+      document.body.scrollLeft = page*(window.outerWidth);
+    }
+  }));
+
+  window.addEventListener("scroll", (function(e) {        
+    document.body.scrollLeft = page*(window.outerWidth);
+  }));
 
 }());
