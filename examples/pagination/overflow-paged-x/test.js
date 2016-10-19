91314d02668b94ed3f26479ab61480f5ab45cc4f
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-19 05:11:23 +0200

    Cycle navigation for paged-x

--- examples/pagination/overflow-paged-x/test.js
+++ examples/pagination/overflow-paged-x/test.js
@@ -7,18 +7,29 @@
   window.addEventListener("click", (function(e) {        
     var clickX = e.pageX;
     console.log("Click detected at: "+clickX);
-    console.log((window.innerWidth*page)+(window.innerWidth/2));
+    //console.log((window.innerWidth*page)+(window.innerWidth/2));
     if (clickX > (window.innerWidth*page)+(window.innerWidth/2)) {
-      page = page+1;
-      document.body.scrollLeft = page*(window.outerWidth);
+      console.log("Current position: " + document.body.scrollLeft);
+      if (document.body.scrollLeft+window.innerWidth == document.body.scrollWidth) {
+        window.scrollTo(0,0);
+        page = 0;
+      } else {
+        page = page+1;
+        document.body.scrollLeft = page*(window.outerWidth);
+      }
     } else {
-      if (page>0) page = page-1;
-      document.body.scrollLeft = page*(window.outerWidth);
+      if (document.body.scrollLeft == 0) {
+        page = document.body.scrollWidth/window.outerWidth;
+        document.body.scrollLeft = page*(window.outerWidth);
+      } else {
+        if (page>0) page = page-1;
+        document.body.scrollLeft = page*(window.outerWidth);
+      }
     }
   }));
 
-  //window.addEventListener("scroll", (function(e) {        
-  //  document.body.scrollLeft = page*(window.outerWidth);
-  //}));
+  window.addEventListener("scroll", (function(e) {        
+    document.body.scrollLeft = page*(window.outerWidth);
+  }));
 
 }());
